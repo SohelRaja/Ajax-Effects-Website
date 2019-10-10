f8a0f31e5e4a112189152538cef3271e2d82d8f4
SohelRaja <rohan.nsec@gmail.com>
2019-10-10 16:13:10 +0530

    blog.php updated with authentication condition

--- blog.php
+++ blog.php
@@ -1,3 +1,9 @@
+<?php 
+    session_start();
+    if(isset($_SESSION['login'])){
+        $fname = $_SESSION['fname'];
+        $lname = $_SESSION['lname'];
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -21,12 +27,6 @@
     <link href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css">
     <link href="http://fonts.googleapis.com/css?family=Josefin+Slab:100,300,400,600,700,100italic,300italic,400italic,600italic,700italic" rel="stylesheet" type="text/css">
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
 
 </head>
 
@@ -43,6 +43,9 @@
         <div class="row">
             <div class="box">
                 <div class="col-lg-12">
+                    <h2 class="text-center">
+                        Welcome <?php echo $fname; echo " "; echo $lname; ?> - <a href="logout.php">Logout</a>
+                    </h2>
                     <hr>
                     <h2 class="intro-text text-center">Avenger
                         <strong>Flims</strong>
@@ -122,3 +125,9 @@
 </body>
 
 </html>
+
+<?php //If User is not logged in then it will redirect to index.php
+    } else {
+        header("location:login.php ");
+    }
+?>
